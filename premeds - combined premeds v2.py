2c33bd5fe29ab2cc27adc966a8f31caeca9a7bf3
progressiverocker <progressive.rocker.email@gmail.com>
2013-09-17 21:15:54 +0100

    Updated based on suggestions from rcuhljr
    also played around with the format of the output

--- premeds - combined premeds v2.py
+++ premeds - combined premeds v2.py
@@ -18,7 +18,6 @@ rates = {
 weight_max = {"dog" : 100.0, "cat" : 15.0, "rabbit" : 15.0}
 valid_species = [ name for name in weight_max.keys() ]
 
-
 def prompt_for_species():
     # force input of species. Keep looping until valid.
     while True:
@@ -31,42 +30,46 @@ def prompt_for_species():
         species_stripped = possible_species[0] if len(possible_species) == 1 else ""
         if species_stripped != "":
             return species_stripped
-            break
-
+        
         else:
-            print("\nOops! Pick one of {}".format(valid_species))
+            print("\n\tOops! Please try again.")
 
-def prompt_for_weight():
+def prompt_for_weight(species_stripped):
     # force input of weight. Keep looping until valid.
     weight_re = re.compile('\d+(\.\d+)?')
-    species_stripped = prompt_for_species()
+
     while True:
-        weight = raw_input("\nEnter the weight in kilograms (eg. 500 grams is '0.5'): ")
+        weight = raw_input("\nEnter the weight for a {} [maximum is {}kg]: ".format(species_stripped, weight_max[species_stripped]))
 
         # valid, but in range?
         if (weight_re.match(weight) != None) and (float(weight) > 0.0) and (float(weight) < weight_max[species_stripped]):
             weight_float = float(weight) 
-
-            # calculate Vet's premed dosage based on animal type
-            # weight * dose_rate  / drug_concentration
-            print("\nCalculate Premedicant Dose for:\n\tSpecies = {} \n\tWeight = {}kg\n".format(species_stripped, weight_float))
-            
-            # iterate every drug, since none was chosen for this animal
-            for species_rates in sorted(rates[species_stripped]):
-                print("\n{}".format(species_rates.title()))
-                for drug_list, dose_rate in enumerate(rates[species_stripped][species_rates]):
-                    value = float(dose_rate)
-                    dosage_calculated = (weight_float * value) / float(drug_concerntrations[species_rates])
-                    print("\tdose rate = {:.2f}ml, calculated dosage = {:.2f}ml".format(dose_rate, dosage_calculated))
+            return weight_float
             break
-
+        
         else:
-            print("\nOops! Weight not in range: 0.0 - {} for a {}".format(weight_max[species_stripped], species_stripped))
+            print("\n\tOops! Please try again.")
 
+def calculate_premeds(species_stripped, weight_float):
+
+    #Calculate Vet's premed dosage based on animal type.
+    #weight * dose_rate  / drug_concentration
+
+    print("\nCalculate Premedicant Dose for:\n\tSpecies = {} \n\tWeight = {}kg\n".format(species_stripped, weight_float))
+
+    # iterate every drug, since none was chosen for this animal
+    for species_rates in sorted(rates[species_stripped]):
+        print("\n\t{}:".format(species_rates.title()))
+        for drug_list, dose_rate in enumerate(rates[species_stripped][species_rates]):
+            value = float(dose_rate)
+            dosage_calculated = (weight_float * value) / float(drug_concerntrations[species_rates])
+            print("\tdose rate = {:.2f}ml, calculated dosage = {:.2f}ml".format(dose_rate, dosage_calculated))
 
 
 restart = "y" 
 while restart == "y":
-    premeds = prompt_for_weight()
-    print premeds
+    species_stripped = prompt_for_species()
+    weight_float
+    = prompt_for_weight(species_stripped)
+    calculate_premeds(species_stripped, weight_float)
     restart = raw_input("\nTO RESTART ENTER 'Y'.")
