55813636c631c86ef66230e08b4b3deda332ec57
progressiverocker <progressive.rocker.email@gmail.com>
2013-09-24 10:52:22 +0100

    added regular expressions
    regex variable to match whole word other matched characters. very sloppy
    but fixed now.

--- UsernamePasswordProtected.py
+++ UsernamePasswordProtected.py
@@ -6,36 +6,39 @@ Have the user and password in a seperate .txt file. Break into your own program.
 import string
 import random
 from random import randrange
+import re
 
-# generates random username and password. Stores in file.
+# randomly generate a username and password in output.txt
 def login():
     username = str(''.join(random.choice(string.ascii_lowercase + string.ascii_uppercase + string.digits) for x in range(randrange(6,10))))
     password = str(''.join(random.choice(string.ascii_lowercase + string.ascii_uppercase + string.digits) for x in range(randrange(6,10))))
+    
     with open(r"Output.txt", "w") as login:
-        data = login.write("username: %s\npassword: %s" % (username, password))
-
+        data = login.write("username %s\npassword %s" % (username, password))
+   
 
 # force input for username and password
 # matches input from user with stored username and password in output.txt
 # loop until correct match on username and password
-
 def prompt():
     
     while True:
         search = open(r"Output.txt")
         username = [line for line in search if "username" in line]
         username = username[0].replace("username", '').strip()
+        regex_username = re.compile(username)
         enter_username = raw_input("\nEnter your username: ").strip()
         
         search = open(r"Output.txt")
         password = [line for line in search if "password" in line]
         password = password[0].replace("password", '').strip()
+        regex_password = re.compile(password)
         enter_password = raw_input("Enter your password: ").strip()
 
     
         if (enter_username != "") and (enter_password != ""):
             
-            if (enter_username in username[0]) and (enter_password in password[0]):
+            if bool(regex_username.search(enter_username)) and bool(regex_password.search(enter_password)):
                 print "\n\tLogin Successful!"
                 break
                 
@@ -46,14 +49,9 @@ def prompt():
             print "\n\tOops! Please try again"
 
 
-
-
-login()
+#login()
 prompt()
 
 raw_input("\nLoading your program ...<Press Enter>")
 
     
-   
-        
-
